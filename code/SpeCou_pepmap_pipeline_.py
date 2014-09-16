7c3990a0654c0a9cb625ecc787ca5db0dde84c73
Milan Simonovic <milan.simonovic@imls.uzh.ch>
2014-09-16 10:13:16 +0200

    refactoring

--- code/SpeCou_pepmap_pipeline_.py
+++ code/SpeCou_pepmap_pipeline_.py
@@ -1,42 +1,103 @@
-# this is for the spectral counting calculating protein abundance and raw spectral count and mapped peptide
-# protocol : input pepetide_count, seq_fasta file -> protein abundance + count -> mapping to internal id ------- input pepetide_count, seq_fasta file -> mapping peptide
+#!/usr/bin/python
+
+# this is for the spectral counting calculating protein abundance
+# and raw spectral count and mapped peptide
+
 
 import sys
 import os
 import shlex
 import subprocess
+from glob import glob
+import re
+import psycopg2
 
+STRING_DB='string_9_0'
 FASTA='../input/v3.0/fasta'
+FASTA_VER='9.0'
+INPUT='../input/v3.0/'
+OUTPUT='../output/v3.0/'
+SPECIES_IDS=[1148, 3702, 4896, 4932, 6239, 7227, 7460, 7955, 8364, 9031, 9606, 9615, 9823, 9913, 10090, 10116, 39947, 64091, 83332, 99287, 160490, 198215, 224308, 267671, 272634, 300267, 449447, 511145, 546414, 593117]
 
-if len(sys.argv) <3:
-    print "need species ID, path, peptide count file to convert as argvment "
-    sys.exit()
-    # filename without ".txt"
-speid = sys.argv[1]
-path = sys.argv[2]
-pepfile = sys.argv[3:]
+def spectral_count_species(species_id):
+    path = INPUT + species_id + '/'
+    for pepfile in glob(path + '*.txt'):
+        print 'processing',pepfile
+        scfile = calculate_abundance_and_raw_spectral_counts(species_id, pepfile)
+        map_peptide(species_id, pepfile)
+        add_string_internalids_column(species_id, scfile)
 
-SCfile = ""
-for f in pepfile:
-    # calculating protein abundance and raw spectral count
-    out = f + ".SC"
-    ofile = open(path +"/"+ out,"w")
-    seqcom1 = "java -Xms512m ComputeAbundanceswithSC -s " + speid + " " + path + "/" + f + ".txt " + FASTA +"/fasta.v9.0." + speid + ".fa"
-    scom1 = shlex.split(seqcom1)
-    subprocess.Popen(scom1, stdout = ofile).wait()
-    ofile.close()
-    SCfile = path + "/" + out + " " 
-    # map peptide
-    
-    out2 = f +"_peptide.txt"
-    ofile2 = open(path + "/" + out2, "a")
-    ofile2.write("#string_external_id	peptide_sequence	spectral_count")
-    seqcom2 = "java -Xms512m ComputeAbundancesMappep -p 4 -s " + speid + " "+ path + "/" + f + ".txt " + FASTA +"/fasta.v9.0." + speid + ".fa |tee > " + out2
-    scom2 = shlex.split(seqcom2)
-    subprocess.Popen(scom2, stdout = ofile2).wait()
-    ofile2.close()    
+def get_filename_no_extension(filename):
+    base = os.path.basename(filename)
+    return os.path.splitext(base)[0]
 
-    # map to string internal id
-    mapcomd = "python map_to_string_ids.py " + speid + " " +SCfile
-    scom3 = shlex.split(mapcomd)
-    subprocess.Popen(scom3).wait()
+def calculate_abundance_and_raw_spectral_counts(speid, pepfile):
+    """
+    takes peptide counts and fasta file and produces protein abundance + counts
+    """
+    cmd = "java -Xms512m ComputeAbundanceswithSC -s {0} {1} {2}/fasta.v{3}.{0}.fa"
+    cmd = cmd.format(speid, pepfile, FASTA, FASTA_VER)
+    scfile = OUTPUT + get_filename_no_extension(pepfile) + ".SC"
+    with open(scfile, "w") as ofile:
+        subprocess.Popen(shlex.split(cmd), stdout = ofile).wait()
+    return scfile
+# inputs: pepetide_counts, seq_fasta file -> protein abundance + count -> mapping to internal id ------- input pepetide_count, seq_fasta file -> mapping peptide
+
+def map_peptide(speid, pepfile):
+    """
+    maps peptides to proteins, takes peptide counts and fasta file 
+    and produces protein/peptide/counts
+    """
+    out = OUTPUT+ get_filename_no_extension(pepfile) + "_peptide.txt"
+    cmd  = "java -Xms512m ComputeAbundancesMappep -p 4 -s {0} {1} {2}/fasta.v{3}.{0}.fa | tee > {4} "
+    cmd  = cmd.format(speid, pepfile, FASTA, FASTA_VER, out)
+
+    with open(out, "a") as ofile:
+        ofile.write("#string_external_id	peptide_sequence	spectral_count\n")
+        ofile.flush()
+        subprocess.Popen(shlex.split(cmd), stdout = ofile).wait()
+
+def load_ids(species_id):
+    print('loading protein mapping for ' + species_id)
+    dbcon = psycopg2.connect("host=imlslnx-eris.uzh.ch port=8182 user=postgres dbname="+STRING_DB)
+    cur = dbcon.cursor()
+    cur.execute("SELECT protein_id, protein_name FROM items.proteins_names WHERE species_id=" + species_id)
+    ids = dict()
+    #print(cur.fetchmany(5))
+    for el in cur:
+        ids[el[1]] = el[0]
+        #    print("\t".join(map(str, el)))
+    cur.close()
+    dbcon.close()
+    return ids
+
+def add_string_internalids_column(species_id, SCfile):
+    """
+    adds another column with STRINGDB internal ids
+    """
+    print('adding string internal ids')
+    ids = load_ids(species_id)
+    #print(",".join(str(x) + " " + str(ids[x]) for x in ids.keys()[:5]))
+
+    with open(SCfile, "r") as inp:
+        with open(SCfile + ".out","w") as out:
+            for line in inp.readlines():
+                if (line.startswith('#')):
+                    out.write(line)
+                else:
+                    rec = re.sub("\\s+", " ", line.strip()).split(" ")
+                    if len(rec) < 2:
+                        continue
+                    if rec[0] not in ids:
+                        sys.stderr.write(rec[0] + "\t" + rec[1]+ "\t" +" in " + f + ": no mapping\n")
+                    else:
+                        newline = str(ids[rec[0]]) + "\t" + species_id + "." + rec[0] + '\t' +  rec[1] +"\t" +rec[2] + "\n"
+                        out.write(newline)
+
+
+
+# main
+if __name__ == "__main__":
+    for species_id in SPECIES_IDS:
+        spectral_count_species(str(species_id))
+#else importing into another module
