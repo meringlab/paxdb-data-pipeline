57730ab3717db9286cd1660713369da0b0b5bab7
Milan Simonovic <mbsimonovic@gmail.com>
2014-10-21 10:39:50 +0200

    add Protein class for StringDb proteins

--- src/sanity_check.py
+++ src/sanity_check.py
@@ -5,19 +5,29 @@ import os
 import glob
 import subprocess
 import logging
+import logger
+import stringdb
+
 
 def check_for_duplicate_entries():
     pass
 
+
 def compile_java_if_necessary():
     for java in glob.glob('*.java'):
-        if not os.path.exists(java.replace('.java','.class')):
-            try: 
-                cmd_out = subprocess.check_output(['javac',java])
+        if not os.path.exists(java.replace('.java', '.class')):
+            try:
+                cmd_out = subprocess.check_output(['javac', java])
             except:
-                logging.error('failed to compile %s: %s',java, cmd_out)
-            
+                logging.error('failed to compile %s: %s', java, cmd_out)
+
+
+def export_from_postgresql():
+    stringdb.export_from_postgresql()
+
 
 if __name__ == '__main__':
+    logger.configure_logging()
+    export_from_postgresql()
     check_for_duplicate_entries()
     compile_java_if_necessary()
