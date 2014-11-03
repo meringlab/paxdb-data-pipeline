e73a4b9c4e137000ffba5ebedc890cb3967c95cc
Milan Simonovic <mbsimonovic@gmail.com>
2014-11-03 14:03:56 +0100

    refactoring

--- src/test_dataset.py
+++ src/test_dataset.py
@@ -1,19 +1,20 @@
-import dataset 
 import unittest
 
+import dataset
+
+
 try:
-    #python2
+    # python2
     from cStringIO import StringIO
 except ImportError:
     from io import StringIO
 
 
 class MapperTest(unittest.TestCase):
-
     def setUp(self):
         self.species = 42
-        self.external_ids_map = {'42.p1': 1, '42.p2': 2, '42.p3' : 3}
-        self.names = {'n11': 1,'n12': 1, 'n2': 2}
+        self.external_ids_map = {'42.p1': 1, '42.p2': 2, '42.p3': 3}
+        self.names = {'n11': 1, 'n12': 1, 'n2': 2}
         self.m = dataset.DatasetMapper(42, self.external_ids_map, self.names)
 
     def test_empty_mapping(self):
@@ -22,40 +23,41 @@ class MapperTest(unittest.TestCase):
 
     def test_all_map_to_external(self):
         e = self.m.map_to_external_ids({'p1': '47.2'})
-        self.assertEquals({'p1': '42.p1'},e)
+        self.assertEquals({'p1': '42.p1'}, e)
 
     def test_mixed_external_names(self):
         e = self.m.map_to_external_ids({'p1': '-1', 'n2': '-1'})
-        self.assertEquals({'p1': '42.p1', 'n2': '42.p2'},e)
+        self.assertEquals({'p1': '42.p1', 'n2': '42.p2'}, e)
 
     def test_names_only(self):
         e = self.m.map_to_external_ids({'n2': '-1'})
-        self.assertEquals({'n2': '42.p2'},e)
+        self.assertEquals({'n2': '42.p2'}, e)
 
     def test_conflict(self):
         e = self.m.map_to_external_ids({'p1': '-1', 'n1': '-1'})
-        self.assertEquals({'p1': '42.p1'},e)
+        self.assertEquals({'p1': '42.p1'}, e)
 
     def test_to_external_id(self):
-        self.assertEquals('12.p1', dataset.to_external_id(12,'p1'))
-        self.assertEquals('12.p1', dataset.to_external_id('12','p1'))
+        self.assertEquals('12.p1', dataset.to_external_id(12, 'p1'))
+        self.assertEquals('12.p1', dataset.to_external_id('12', 'p1'))
+
 
 class EntriesReaderTest(unittest.TestCase):
     def test_single_entry(self):
         e = dataset.read_entries(StringIO('p1\t12\n\n'))
         self.assertEquals(1, len(e))
-        self.assertEquals('12',e['p1'])
+        self.assertEquals('12', e['p1'])
 
     def test_multiple_entries(self):
         e = dataset.read_entries(StringIO('p1\t12\np2\t99.1\t47'))
         self.assertEquals(2, len(e))
-        self.assertEquals('12',e['p1'])
-        self.assertEquals('99.1\t47',e['p2'])
-    
+        self.assertEquals('12', e['p1'])
+        self.assertEquals('99.1\t47', e['p2'])
+
     def test_skip_headers(self):
         e = dataset.read_entries(StringIO(' # a 1\n#b 2 2\np1\t12\n'))
         self.assertEquals(1, len(e))
-        self.assertEquals('12',e['p1'])
+        self.assertEquals('12', e['p1'])
 
 
 if __name__ == '__main__':
