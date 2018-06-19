e5649228d5b4d4d0b3c1f1c4090d6fb87c25a7db
Milan Simonovic <mbsimonovic@gmail.com>
2018-06-19 10:40:58 +0200

    add missing input files

---
+++ input/v4.1/datasets/.scripts/format_dataset_info.py
@@ -0,0 +1,66 @@
+
+
+#!/usr/bin/python
+#
+# small util script to dump new datasets info
+# that can be copy/pasted into the google doc
+# 
+import sys
+
+# first arg is the folder containing datasets
+dir=sys.argv[1]
+# config:
+DB_URL='host=imlslnx-eris.uzh.ch port=8182 user=postgres dbname=string_10_0'
+
+## 
+import os
+import glob
+import re
+import psycopg2
+
+species_ids=[353153,4932  ,511145, 243159]
+# for spc in os.listdir(dir):
+#     if spc.isdigit():
+#         species_ids.append(spc)
+
+
+NAMES=dict()
+GENOME_SIZE=dict()
+dbcon = psycopg2.connect(DB_URL)
+cur = dbcon.cursor()
+cur.execute('''
+  SELECT s.species_id, s.compact_name, count(p.protein_id)
+  FROM items.species as s, items.proteins as p
+  WHERE p.species_id = s.species_id
+  AND s.species_id in %s
+  GROUP BY s.species_id, s.compact_name;
+''', [tuple(int(x) for x in species_ids)] )
+
+#  list(map(lambda x: int(x), species_ids)))
+
+for el in cur:
+    NAMES[str(el[0])] = el[1]
+    GENOME_SIZE[str(el[0])] = el[2]
+cur.close()
+dbcon.close()
+
+def count_entries(from_file):
+    protein_number = 0
+    with open(from_file) as file:
+        for line in file:
+            if re.match('\\s*#.*', line):  # line startwith '#'
+                continue
+            rec = re.sub("\\s+", " ", line.strip()).split(" ") #empty line
+            if len(rec) < 2:
+                continue
+            protein_number = protein_number + 1
+
+for sid in species_ids:
+    spc = str(sid)
+    dsetdir = os.path.join(dir,str(spc))
+    for d in os.listdir(dsetdir):
+        name = NAMES[spc] if spc in NAMES else '-'
+        print("{0}\t{1}\t{2}\t0\t100\t{3}\t{4}\twhole_organism\tN\tTODO\tTODO\tTODO\tMAPPED_BY_AUTHORS".format(spc, name, d, 0, GENOME_SIZE[spc]))
+
+
+
---
+++ input/v4.1/fasta/remove_species_id.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import os
+import glob
+
+for tsv in glob.glob("*tsv"):
+    print("processing ", tsv)
+    file = open(tsv)
+
+    rewritten_file = os.path.splitext(os.path.splitext(tsv)[0])[0] + '.network_v10_900.txt'
+    if os.path.isfile(rewritten_file):
+        continue
+    with open(rewritten_file, 'w') as rewritten:
+        for line in file:
+            if  line.startswith('protein1'):
+                continue
+            if line.startswith('#') or line.startswith('protein1'):
+                rewritten.write(line)
+                continue
+            r = line.split(' ')
+            if len(r) < 2: # empty line?
+                continue
+            score = int(r[2].strip())
+#            score = int(float(r[2].strip()) * 1000)
+            if score < 900:
+                continue
+            rewritten.write(r[0][r[0].index('.') + 1:])
+            rewritten.write('\t')
+            rewritten.write(r[1][r[1].index('.') + 1:])
+            rewritten.write('\t')
+            rewritten.write(str(score))
+            rewritten.write('\n')
+    file.close()
